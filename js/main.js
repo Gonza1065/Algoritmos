4e0776ebf25edfff9d3714b8f37c21c4844f6094
Gonzalo Saguas <desarrollosaguas@gmail.com>
2022-06-24 18:44:06 -0300

    algoritmo

---
+++ js/main.js
@@ -0,0 +1,27 @@
+console.log('Bienvenido al algoritmo de ciclos');
+// for (let i = 0; i <= 10; i++ ) {
+//     let ingresarNumero = prompt ("Ingresar su numero");
+//     alert ("Su numero es " + ingresarNumero);
+
+// }
+
+let tabla = 2
+let resultado; 
+for (let i= 1; i <= 10; i++) {
+    let ingresarNumero = prompt ("Ingresar su numero");
+    alert ("Su numero es " + ingresarNumero);
+    resultado = tabla * ingresarNumero;
+    console.log(ingresarNumero + " x " + tabla + " es igual a " + resultado);
+}
+
+
+
+
+
+
+// let tabla = 2;
+// let resultado;
+// for (let i = 1; i <= 10; i++) {
+//     resultado = tabla * i;
+//     console.log(tabla + " x " + i + " es igual a " + resultado);
+// }
